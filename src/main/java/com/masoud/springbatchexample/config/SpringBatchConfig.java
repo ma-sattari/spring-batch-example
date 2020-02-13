172216097609250b234c63bf5b83323442ce6db8
Masoud Sattari <masoud.sattari@argedor.com>
2020-02-13 11:23:56 +0300

    Update code, TODO downgrade Spring version to 1.15

--- src/main/java/com/masoud/springbatchexample/config/SpringBatchConfig.java
+++ src/main/java/com/masoud/springbatchexample/config/SpringBatchConfig.java
@@ -1,19 +1,35 @@
 package com.masoud.springbatchexample.config;
 
+import com.masoud.springbatchexample.model.User;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SuppressWarnings("SpringJavaAutowiringInspection")
 public class SpringBatchConfig {
 
+    //https://www.youtube.com/watch?v=1XEX-u12i0A
     @Bean
-    public Job job(JobBuilderFactory jobBuilderFactory){
+    public Job job(JobBuilderFactory jobBuilderFactory,
+                   StepBuilderFactory stepBuilderFactory,
+                   ItemReader<User> itemReader,
+                   ItemProcessor<User,User> itemProcessor,
+                   ItemWriter<User> itemWriter){
 
-        Step step = null;
+        Step step = stepBuilderFactory.get("ETL-file-load")
+                .chunk(100)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
         return jobBuilderFactory.get("ETL-Load")
                 .incrementer(new RunIdIncrementer())
                 .start(step)
